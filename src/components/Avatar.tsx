224c2e0dafb05d7b16e76f9c966209b8bb04733d
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-07-16 04:34:06 +0700

    fix: add tailwindcss to vite config

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,10 @@
 import myAvatar from '../img/avatar.png'
 function Avatar() {
   return (
-    <div className='card w-48 h-[8.5rem] items-center p-5 bg-zinc-800 hidden md:block rounded-lg'>
+    <div className='card w-48 h-48 items-center p-5 bg-zinc-800 hidden md:block rounded-full'>
       <div className="avatar flex justify-center">
-        <div className='w-24 rounded-full'>
-          <img src={myAvatar} alt=""/>
+        <div className='w-48 h-48 rounded-full'>
+          <img src={myAvatar} alt="" className='rounded-full'/>
         </div>
       </div>
     </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import Menu from "../components/Menu";
 
 function MainPage({ page }: { page: string }) {
   return (
-    <div className="flex justify-center py-[5rem] m-auto bg-red-50">
+    <div className="bg-[#111111] flex justify-center py-[5rem] m-auto text-white">
       <div className="w-[70%] flex flex-col md:justify-between m-auto gap-5 min-h-screen h-fit md:flex-row">
         <Menu page={page} />
         <Main page={page} key={page} />
